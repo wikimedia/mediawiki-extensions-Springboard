f0713483a115107fa412c275599595600f216da1
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2025-05-10 04:18:23 +0530

    Add wikidata label and description integration

--- resources/components/Extensions.vue
+++ resources/components/Extensions.vue
@@ -6,8 +6,8 @@
         :paginate="true"
 	>
         <template #item-name="{ item }">
-			<a :href="`https://www.mediawiki.org/wiki/Extension:${ item }`">{{ item }}</a>
-		</template>
+          <a :href="`https://www.mediawiki.org/wiki/Extension:${ item }`">{{ item }}</a>
+        </template>
         <template #item-action="{ item }">
 			<cdx-button v-if="item.exists" action="destructive" weight="primary" @click="submit(item)">{{ item.action }}</cdx-button>
 			<cdx-button v-else action="progressive" weight="primary" @click="submit(item)">{{ item.action }}</cdx-button>
@@ -16,8 +16,42 @@
 </template>
 
 <script>
-const { ref } = require( 'vue' );
+const { ref, onMounted } = require( 'vue' );
 const { CdxTable, CdxButton } = require( '../codex.js' );
+
+const chunkArray = (array, size) => {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += size) {
+    chunks.push(array.slice(i, i + size));
+  }
+  return chunks;
+};
+
+const fetchWikidataMetadata = async (ids, userLang) => {
+  const endpoint = 'https://www.wikidata.org/w/api.php';
+  const params = new URLSearchParams({
+    action: 'wbgetentities',
+    ids: ids.join('|'),
+    format: 'json',
+    props: 'labels|descriptions',
+    languages: `${userLang}|en`,
+    origin: '*'
+  });
+
+  const response = await fetch(`${endpoint}?${params.toString()}`);
+  const data = await response.json();
+
+  const result = {};
+  for (const [id, entity] of Object.entries(data.entities)) {
+    const labels = entity.labels || {};
+    const descriptions = entity.descriptions || {};
+    result[id] = {
+      label: labels[userLang]?.value || labels.en?.value || '',
+      description: descriptions[userLang]?.value || descriptions.en?.value || ''
+    };
+  }
+  return result;
+};
 
 // @vue/component
 module.exports = {
@@ -27,39 +61,62 @@ module.exports = {
         CdxButton
     },
 	setup() {
-        let data = mw.config.get( 'WTExtensions' );
-        let version = mw.config.get( 'wgVersion' ).split( '.' );
-		const mwVersion = `REL${version[0]}_${version[1]}`;
-        data = data.map( (key) => {
-            const extName = Object.keys(key)[0];
-            updatedMap = { ...Object.values(key)[0], name: extName };
-            if ( !( 'branch' in updatedMap ) ) {
-                updatedMap[ 'branch' ] = mwVersion;
-            }
-            if ( !( 'commit' in updatedMap ) ) {
-                updatedMap[ 'commit' ] = "LATEST";
-            }
-            let mapCopy = {...updatedMap};
-            let installActionName = "Install";
-            let uninstallActionName = "Uninstall";
-            if ( 'bundled' in updatedMap ) { 
-                installActionName = 'Enable';
-                uninstallActionName = 'Disable';
-            }
-            updatedMap['action'] = updatedMap['exists']
-                ? { ...mapCopy, action: uninstallActionName }
-                : { ...mapCopy, action: installActionName }
-            return updatedMap;
-        } );
-		return {
-            'data': data,
-            'columns': [
-                {id: 'name', label: 'Extension Name', sortable: true},
-                {id: 'commit', label: 'Commit', sortable: true},
-                {id: 'branch', label: 'Branch', sortable: true},
-                {id: 'action', label: 'Action', sortable: true}
-            ]
-        };
+    let data = mw.config.get( 'WTExtensions' );
+    const finalData = ref([]);
+    const userLang = mw.config.get( 'wgUserLanguage' );
+    let version = mw.config.get( 'wgVersion' ).split( '.' );
+    const mwVersion = `REL${version[0]}_${version[1]}`;
+    const wikidataIds = data
+      .map(obj => Object.values(obj)[0].wikidataid)
+      .filter(Boolean);
+    onMounted(async () => {
+      const chunks = chunkArray(wikidataIds, 50);
+      let mergedData = {};
+          for (const chunk of chunks) {
+              const metadata = await fetchWikidataMetadata(chunk, userLang);
+              Object.assign(mergedData, metadata);
+          }
+      data = data.map( (key) => {
+          const wikidataid = Object.values(key)[0].wikidataid;
+          const name = Object.keys(key)[0];
+          const meta = mergedData[wikidataid] || {};
+          const updatedMap = { ...Object.values(key)[0],
+              name: meta.label || name,
+              desc: meta.description || ""
+          };
+          return updatedMap;
+      } );
+      data = data.map( (updatedMap) => {
+          if ( !( 'branch' in updatedMap ) ) {
+              updatedMap[ 'branch' ] = mwVersion;
+          }
+          if ( !( 'commit' in updatedMap ) ) {
+              updatedMap[ 'commit' ] = "LATEST";
+          }
+          let mapCopy = {...updatedMap};
+          let installActionName = "Install";
+          let uninstallActionName = "Uninstall";
+          if ( 'bundled' in updatedMap ) { 
+              installActionName = 'Enable';
+              uninstallActionName = 'Disable';
+          }
+          updatedMap['action'] = updatedMap['exists']
+              ? { ...mapCopy, action: uninstallActionName }
+              : { ...mapCopy, action: installActionName }
+          return updatedMap;
+      } );
+      finalData.value = data;
+    });
+    return {
+      'data': finalData,
+      'columns': [
+        {id: 'name', label: 'Extension Name', sortable: true},
+        {id: 'desc', label: 'Description', sortable: true},
+        {id: 'commit', label: 'Commit', sortable: true},
+        {id: 'branch', label: 'Branch', sortable: true},
+        {id: 'action', label: 'Action', sortable: true}
+      ]
+    };
 	},
     methods: {
         submit ( data ) {
