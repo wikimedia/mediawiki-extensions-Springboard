d65e82edfec4b3684c664ca1c3c4101968016652
TechieNK <nk2indian@gmail.com>
2025-05-26 21:16:27 +0530

    Change "no-data-message" to "No configuration found"

--- resources/components/Skins.vue
+++ resources/components/Skins.vue
@@ -189,7 +189,7 @@ module.exports = {
             searchString,
             search,
             'searchPlaceholder': mw.msg('springboard-skins-tab-search-placeholder'),
-            'noDataMsg': mw.msg('springboard-no-data-message'),
+            'noDataMsg': mw.msg('springboard-no-data', mw.config.get( 'wgVersion' )),
             sort,
             onSort,
             isFetched,
